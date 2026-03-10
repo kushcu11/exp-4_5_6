2f74903b2b35bb95374b8c0bbee7f0cd531b2995
Kush <ivanatinkledrexel@gmail.com>
2026-03-10 09:44:35 +0530

    Add files via upload

---
+++ UI-Design-Using-Component-Libraries-main/Experiment-5/src/User.jsx
@@ -0,0 +1,15 @@
+function User() {
+  return (
+    <div>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+      <h1>User name is Aamir Ekbal</h1>
+    </div>
+  );
+}
+
+export default User;
